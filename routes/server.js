74ec8671114b3a38a3fa54ef0188387fc5135349
dez03 <hernandezaviel@gmail.com>
2025-05-27 17:54:21 -0700

    added display message

--- routes/server.js
+++ routes/server.js
@@ -84,3 +84,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('🛠️ GT Coaching Backend is Live!');
+});
